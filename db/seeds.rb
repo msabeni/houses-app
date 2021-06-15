2d0afa81a145269ffe1cd20a69d81de453d05c73
msabeni <kemimsabeni@gmail.com>
2021-06-15 12:39:07 +0300

    seed the db with some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+House.create([
+    { 
+      name: "Phil Studio Apartment",
+      image_url: "https://www.pexels.com/photo/house-lights-turned-on-106399/"
+    }, 
+    { 
+      name: "Romtech Apartment ",
+      image_url: "https://www.pexels.com/photo/beautiful-wooden-houses-with-lawns-1643389/"
+    },
+    { 
+      name: "Easy Pack",
+      image_url: "https://www.pexels.com/photo/white-2-storey-house-near-trees-1115804/" 
+    }, 
+    { 
+      name: "Alma Home",
+      image_url: "https://www.pexels.com/photo/lighted-beige-house-1396132/" 
+    }, 
+    { 
+      name: "Jamhuri Homes",
+      image_url: "https://www.pexels.com/photo/house-on-green-landscape-against-sky-314937/" 
+    }, 
+    { 
+      name: "Becky Apartments",
+      image_url: "https://www.pexels.com/photo/multicolored-wooden-houses-2374652/" 
+    }
+  ])
